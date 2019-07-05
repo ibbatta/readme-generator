3224c6c9caea05e9e5d860f702fff3eee6434acb
Maurizio Battaghini <maurizio.battaghini+tech@gmail.com>
2019-07-05 11:07:42 +0200

    resolve merge conflicts and fix the husky hooks problems (cause by and old git version: >2.13 must be installed)

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const firstQuestion = () => {
       name: 'firstname',
       type: 'input',
       message: 'Insert first name:'
+    },
+    {
+      name: 'lastname',
+      type: 'input',
+      message: 'Insert last name:'
     }
   ];
   console.log(chalk.blueBright('First settings:'));
