e95d43eeb97a42f9cb5d09890b89433d796b47cd
Maurizio Battaghini <maurizio.battaghini+tech@gmail.com>
2019-07-23 17:54:08 +0200

    parsing error

--- settings/message.settings.js
+++ settings/message.settings.js
@@ -6,12 +6,12 @@ import Boxen from 'boxen';
 const Log = showData => console.log(showData);
 
 const labels = {
-  mainTitle: '%s',
-  questionTitle: '%s',
-  genericError: '%s',
-  readFileError: 'ERROR: The %s file is missing or not readable',
-  writeFileError: 'ERROR: Unable to generate the %s file',
-  writeFileSuccess: 'The %s file is generated with success'
+  mainTitle: '%1$s',
+  questionTitle: '%1$s',
+  genericError: '%1$j',
+  readFileError: 'ERROR: The %1$s file is missing or not readable.\n%2$j',
+  writeFileError: 'ERROR: Unable to generate the %1$s file.\n%2$j',
+  writeFileSuccess: 'The %1%s file is generated with success'
 };
 
 const mainTitle = data => {
@@ -22,12 +22,12 @@ const questionTitle = data => {
   Log(Chalk.blueBright(Sprf(labels.questionTitle, data)));
 };
 
-const genericError = data => {
-  Log(Chalk.red(Sprf(labels.genericError, data)));
+const genericError = error => {
+  Log(Chalk.red(Sprf(labels.genericError, error)));
 };
 
-const readFileError = data => {
-  Log(Chalk.red(Sprf(labels.readFileError, Chalk.bold.underline(data))));
+const readFileError = (data, error) => {
+  Log(Chalk.red(Sprf(labels.readFileError, Chalk.bold.underline(data), error)));
 };
 
 const writeFileSuccess = data => {
@@ -42,8 +42,10 @@ const writeFileSuccess = data => {
   );
 };
 
-const writeFileError = data => {
-  Log(Chalk.red(Sprf(labels.writeFileError, Chalk.bold.underline(data))));
+const writeFileError = (data, error) => {
+  Log(
+    Chalk.red(Sprf(labels.writeFileError, Chalk.bold.underline(data), error))
+  );
 };
 
 export default {
